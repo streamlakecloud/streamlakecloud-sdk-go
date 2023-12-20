a23482b3bb9eda9fba5f0382dfc17c98f41b62f7
yuchenglong <yuchenglong@kuaishou.com>
2023-12-20 18:09:56 +0800

    增加Pcdn带宽用量接口

--- service/metric/client.go
+++ service/metric/client.go
@@ -11,6 +11,13 @@ func NewMetricClient(httpClient base.HTTPClient) *MetricClient {
 		base.NewClient(httpClient, ServiceInfo, ApiList),
 	}
 }
+
+func NewMetricClientV2(httpClient base.HTTPClient, serviceInfo base.ServiceInfo) *MetricClient {
+	return &MetricClient{
+		base.NewClient(httpClient, serviceInfo, ApiList),
+	}
+}
+
 func (v *MetricClient) DescribeDomainUsageData(req DescribeDomainUsageDataRequest) (*DescribeDomainUsageDataResponse, error) {
 	resp := &DescribeDomainUsageDataResponse{}
 	err := v.PostForAPIWithRequestResponse("DescribeDomainUsageData", req, resp)
@@ -32,6 +39,25 @@ func (v *MetricClient) DescribeMpsUsageData(req DescribeMpsUsageDataRequest) (*D
 func (v *MetricClient) DescribeStorageUsageData(req DescribeStorageUsageDataRequest) (*DescribeStorageUsageDataResponse, error) {
 	resp := &DescribeStorageUsageDataResponse{}
 	err := v.PostForAPIWithRequestResponse("DescribeStorageUsageData", req, resp)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (v *MetricClient) DescribePcdnUsageData(req DescribePcdnUsageDataRequest) (*DescribePcdnUsageDataResponse, error) {
+	// Pcdn 接口底层复用cdn接口
+	cdnReq := DescribeCdnUsageDataRequest{
+		DomainName:     req.DomainName,
+		StartTime:      req.StartTime,
+		EndTime:        req.EndTime,
+		Interval:       req.Interval,
+		Percentile:     req.Percentile,
+		AccelerateType: "PCDN",
+		AggregateType:  req.AggregateType,
+	}
+	resp := &DescribePcdnUsageDataResponse{}
+	err := v.PostForAPIWithRequestResponse("DescribeDomainUsageData", cdnReq, resp)
 	if err != nil {
 		return nil, err
 	}
--- service/metric/model.go
+++ service/metric/model.go
@@ -40,3 +40,28 @@ type DescribeStorageUsageDataResponse struct {
 	ResponseMeta *base.ResponseMeta
 	ResponseData interface{} `json:",omitempty"`
 }
+
+type DescribePcdnUsageDataRequest struct {
+	DomainName    string // 实验分组域名标识。若参数为空，默认返回所有实验分组域名合并后数据。支持批量查询(去重)，多个用半角逗号（,）分隔。
+	StartTime     string // 获取数据起始时间点。格式为：yyyy-MM-ddTHH:mm:ssZ（UTC时间）。
+	EndTime       string // 获取数据结束时间点，需晚于起始时间。格式为：yyyy-MM-ddTHH:mm:ssZ（UTC时间）。
+	Interval      string // 统计时间粒度。取值：5minutes：5分钟 (最大时间范围1天,最长过去100天) hour：小时。(最大时间范围7天,最长过去100天) day：天。(最大时间范围31天, 最长过去100天)默认按时间跨度决定，小于等于1小时以5分钟为粒度，小于等于1天以1小时为粒度，大于1天则以天为粒度。
+	Percentile    string // 查询95峰值, 可选值：DomesticBps, OverseaBps, TotalBps, 返回值在返回记录UsageDataItem对应字段中
+	AggregateType string // 聚合类型，数据结果聚合处理，有效值：Normal：查询时间95峰值数据，Avg：查询时间段内日95峰值的均值数据，默认为Normal，备注：只有传Percentile参数时，AggregateType才会生效
+}
+
+type DescribePcdnUsageDataResponse struct {
+	ResponseMeta *base.ResponseMeta
+	ResponseData interface{} `json:",omitempty"`
+}
+
+type DescribeCdnUsageDataRequest struct {
+	DomainName     string // 加速域名。若参数为空，默认返回所有加速域名合并后数据。支持批量查询(去重)，多个用半角逗号（,）分隔。(主域名)
+	StartTime      string // 获取数据起始时间点。格式为：yyyy-MM-ddTHH:mm:ssZ（UTC时间）。
+	EndTime        string // 获取数据结束时间点，需晚于起始时间。格式为：yyyy-MM-ddTHH:mm:ssZ（UTC时间）。
+	Interval       string `json:"Interval,omitempty"`   // 统计时间粒度。取值：5minutes：5分钟 (最大时间范围1天,最长过去7天) hour：小时。(最大时间范围7天,最长过去30天) day：天。(最大时间范围30天, 最长过去90天)默认按时间跨度决定，小于等于1小时以5分钟为粒度，小于等于1天以1小时为粒度，大于1天则以天为粒度。
+	Percentile     string `json:"Percentile,omitempty"` // 查询95峰值, 可选值：DomesticBps, OverseaBps, TotalBps, 返回值在返回记录UsageDataItem对应字段中
+	AccelerateType string // 加速类型。可选值：CDN、PCDN，默认是CDN，控制查询CDN用量或者PCDN用量
+	AggregateType  string // 聚合类型，数据结果聚合处理，有效值：Normal：查询时间95峰值数据，Avg：查询时间段内日95峰值的均值数据，默认为Normal，备注：只有传Percentile参数时，AggregateType才会生效
+	SpaceName      string `json:"SpaceName,omitempty"` // 空间名称，不传按全部空间数据查询
+}
